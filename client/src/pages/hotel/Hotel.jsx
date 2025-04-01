71ac85302ec3f75179b0cf714224b79402ff41a2
Vineetha1312 <113093447+Vineetha1312@users.noreply.github.com>
2025-04-01 12:08:24 +0530

    Update Hotel.jsx

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -24,7 +24,7 @@ const Hotel = () => {
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
-  const { data, loading, error } = useFetch(`https://bookingapp-api-gray.vercel.app/hotels/find/${id}`);
+  const { data, loading, error } = useFetch(`/hotels/find/${id}`);
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
